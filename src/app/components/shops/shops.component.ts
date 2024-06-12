71bc2e28095e23986e44e52cf1ce45492b6785bb
aleira <aleira.redfury@gmail.com>
2024-06-12 10:02:34 +0200

    wip resolved shops and atuth service

--- src/app/components/shops/shops.component.ts
+++ src/app/components/shops/shops.component.ts
@@ -1,41 +1,40 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductsService } from '../../services/products.service';
 import { Shop } from '../../interfaces/shop';
 // import items from '../data/products';
 
 @Component({
-  selector: 'app-shops',
+  selector: 'shop',
   standalone: true,
-  imports: [ NgFor, NgIf, FormsModule, ShopsComponent],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './shops.component.html',
   styleUrl: './shops.component.css',
 })
 export class ShopsComponent implements OnInit {
-
-  products: Shop[] = [];
+  @Input() shop: Shop[] = [];
   filteredShops: Shop[] = [];
   filtro: string = '';
+
+  @Output() myEvent = new EventEmitter<string>();
+  handleEvent(event: string) {
+    console.log(event);
+  }
 
   constructor(private productService: ProductsService) {}
 
   ngOnInit() {
     // Carica i prodotti all'inizializzazione
     this.productService.getProducts().subscribe((data) => {
-      this.products = data;
+      this.shop = data;
       this.filteredShops = data;
     });
 
-    this.productService.search$.subscribe(term => {
-      this.filteredShops = this.products.filter(shop => 
+    this.productService.search$.subscribe((term) => {
+      this.filteredShops = this.shop.filter((shop) =>
         shop.denominazione.toLowerCase().includes(term.toLowerCase())
       );
     });
   }
-
-
-handleEvent(event: string) {
-  console.log(event);
-}
 }
